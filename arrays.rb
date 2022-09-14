eeee08974f0bd4ad6dc1e0756e81d749260c172c
Thomas Casady <tscasady@gmail.com>
2022-09-14 10:03:39 -0500

    Add 1 method and explanation for each array

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ my_strings = ['hello', 'world', 'string', 'exampe']
 my_ints = [1, 2, 3, 4]
 my_floats = [1.1, 2.2, 3.3, 4.4]
 my_bools = [true, false, true, false]
+
+#This .pop method will remove and return the last element of the my_strings array
+my_strings.pop
+
+#This .shift method will remove and return the first element of the my_ints array
+my_ints.shift
+
+#This .push method will add the argument (5.5) as an element to the end of the my_floats array
+my_floats.push(5.5)
+
+#This .unshift method will add the argument (false) to the beginning of the my_bools array
+my_bools.unshift(false)
